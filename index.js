8a527d15a09b7f89f9753da0688bb586474c2089
Omar <omartsadek@gmail.com>
2022-04-25 12:56:27 -0400

    commit tree 75d3c416cddb5d3a97ab9f5376d5426d9d5c0cd4

---
+++ index.js
@@ -0,0 +1,42 @@
+const divide = (num1 = 2000, num2 = 100) => num1/num2
+const square = num1 => num1 * num1
+const add = (num1, num2) => num1 + num2
+
+
+
+
+
+
+
+
+
+
+
+// function divide(num1,num2){
+//     num1 = 2000;
+//     num2 = 100;
+//         return num1/num2
+// }
+
+// const square = function(num1){
+//     return num1*num1
+// }
+
+// const add = function(num1,num2){
+//     num1 = 3
+//     num2 = 4 
+//         return num1 + num2
+// }
+
+
+
+// function divide(){          //has a function expression called divide
+//     return num1/num2
+// }   
+// const num1 = 2000;
+// const num2 = 100;
+//     divide(num1,num2);      //divide divides 2000 by 100
+
+// const square = (parameter1) => parameter1*parameter1;       //square arrow function takes one parameter and multiplies it times itself
+// const add = (parameter1, parameter2) => parameter1 + parameter2;      //add arrow function takes two parameters and adds them together  
+                                                                        
